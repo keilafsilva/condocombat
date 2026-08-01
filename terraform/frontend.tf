a479c016c754a058e098649ba3c7b8a0545634f5
Keila Silva <keilafsilva96@gmail.com>
2026-08-01 17:30:35 -0300

    desafio 3 - teste

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -1,17 +1,17 @@
-resource "render_service" "frontend" {
-  name    = "condocombat-frontend-web"
-  type    = "web_service"
-  env     = "image"
-  region  = "oregon"
-  plan    = "free"
+resource "render_web_service" "frontend" {
+  name   = "condocombat-frontend-web"
+  region = "oregon"
+  plan   = "free"
 
-  image = {
-    image_url = "docker.io/${var.dockerhub_username}/condocombat-frontend:latest"
+  runtime_source = {
+    image = {
+      image_url = "docker.io/${var.dockerhub_username}/condocombat-frontend:latest"
+    }
   }
 
   env_vars = {
     "NEXT_PUBLIC_API_URL" = {
-      value = render_service.backend.url
+      value = render_web_service.backend.url
     }
     "PORT" = {
       value = "3000"
