c4fe5161e4dcb8c3ce35742bdac257c83e038708
Keila Silva <keilafsilva96@gmail.com>
2026-08-01 16:55:13 -0300

    desafio 3 - teste

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,5 +1,5 @@
 resource "supabase_project" "db" {
-  organization_id   = "sua-org-id-supabase"
+  organization_id   = "aodzgmqwcdlasptlpdin"
   name              = "condocombat-db"
   database_password = var.supabase_db_password
   region            = "us-east-1"
