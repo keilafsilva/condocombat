6fe0a3cdaf0e3309523e03fd3b791ce5cb212d91
Keila Silva <keilafsilva96@gmail.com>
2026-08-10 13:18:33 -0300

    feat: add deploy

---
+++ terraform/outputs.tf
@@ -0,0 +1,12 @@
+
+output "backend_url" {
+  value = render_service.backend.url
+}
+
+output "frontend_url" {
+  value = render_service.frontend.url
+}
+
+output "landing_url" {
+  value = "https://${data.netlify_site.landing.name}.netlify.app"
+}
